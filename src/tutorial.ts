791cbc07345d1a3ea60b93aea4fb6b0219e60ff2
emmeiwhite <emmeiwhite@gmail.com>
2024-09-04 21:54:06 +0530

    interface example and build

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,2 +1,14 @@
 // We need to import the file in main.ts for the file to be executed!!
 console.log('TypeScript is awesome!')
+
+interface someValue {
+  name: string
+  age: number
+}
+
+let person: someValue = {
+  name: 'Shakeel Bhai',
+  age: 31
+}
+
+console.log(person)
