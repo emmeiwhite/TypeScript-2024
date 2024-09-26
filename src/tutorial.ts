ce0e40a22177118b66960247bed0355e5c2c7c44
emmeiwhite <emmeiwhite@gmail.com>
2024-09-26 11:29:41 +0530

    Union Types Revisit, Array & Objects

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -34,3 +34,123 @@ let color: string = 'white'
 // color = 35
 
 console.log(color)
+
+// Day-1 Resuming learning (Wed 25-2024) Move on "Himmate Mardaan MadDade Khuda!"
+
+// 1. Union types:  With pipe | to hold multiple types for a variable (UseCase is when we receive API data & we are not sure about the type)
+
+let tax: string | number
+
+tax = 12
+tax = '$12'
+
+// 2. literal type
+let responseStatus: 'pending' | 'success' | 'failure'
+responseStatus = 'success'
+responseStatus = 'failure'
+
+// responseStatus = 'random'
+
+// 3. any
+
+let jobStatus: any = 'moving forward' // Remember TypeScript infers the type
+jobStatus = 23
+
+/** --- Note: Be aware of type any, because it can spread like wild fire and such an approach somewhat defeats the entire purpose of using typescript capabilities of introduction of types to avoid errors during the production --- */
+
+// Example-1:
+
+const books = ['1984', 'Brave New World', 'Fahrenheit 451']
+
+let foundBook: undefined | string
+
+// This piece of code will execute on the run-time. So for TypeScript during development, foundBook is undefined & on line 76, when we try foundBook. we notice the error message: foundBook is possibly 'undefined'
+for (let book of books) {
+  if (book === '1984') {
+    foundBook = book
+    break
+  }
+}
+
+// foundBook.
+
+/** Day-2: UnionChallenge */
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing'
+orderStatus = 'shipped'
+
+// orderStatus = 'cancelled' This cannot be done
+
+// Array & Objects
+
+let temperatures: number[] = [25, 27, 30]
+temperatures.push(-23)
+
+// temperatures = 34
+// temperatures.push('hot')
+
+let colors: string[] = ['red', 'orange']
+colors.push('blue')
+
+// colors.push(true)
+
+let mixed: (string | number)[] = ['apple', 24, 'price', 500]
+mixed.push('pen', 30)
+
+//Do not do the following
+// let test1:[]=[23,45,56]; because test1 can only be an empty array now test1 = []
+
+/** --- Object --- */
+let car: { brand: string; year: number } = { brand: 'toyoto', year: 2020 }
+
+// car.color = 'blue'; It will not work, since color property type is not annotated in the car
+
+/** ----- Objects in an Array Example --- */
+let notebook = { title: 'classmate', price: 90 }
+let pen = { title: 'reynold', price: 10 }
+let eraser = { title: 'classmate' }
+
+// Now we want to save all 3 objects in a array
+
+let studentItems: { title: string; price?: number }[] = [notebook, pen, eraser]
+// Here we used optional chaining ? operator and which makes the price optional now
+
+// Similarly we an make the array to be read only
+let student2Items: { readonly title: string; price?: number }[] = [pen, eraser]
+// student2Items[0].title = 'schoolmate'; This is not possible because the array is readonly
+
+/** --- SIMPLE CHALLENGE ---*/
+let bike: {
+  brand: string
+  year: number
+} = { brand: 'yamaha', year: 2010 }
+
+let laptop: {
+  brand: string
+  year: number
+} = { brand: 'dell', year: 2020 }
+
+/*
+let laptop2: {
+  brand: string
+  year: number
+} = { brand: 'dell' }
+There has to be a year property in laptop2 object, otherwise typescript will scream 
+*/
+
+let laptop3: {
+  brand: string
+  year?: number
+} = { brand: 'dell' }
+
+// laptop3.year = '2020'; Again though the property 'year' is optional, it's type must be number
+laptop3.year = 2020
+
+// Challenge-2: Adding products to an array
+
+let product1 = { title: 'bed', price: 15000 }
+let product2 = { title: 'water cane', price: 85 }
+let product3 = { title: 'silver ring' }
+
+let latestProducts: { title: string; price?: number }[] // here we declared an array of objects
+
+latestProducts = [product1, product2, product3]
