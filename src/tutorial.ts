f72d0a1992297e6ec60066c2afc76759256f7f85
emmeiwhite <emmeiwhite@gmail.com>
2024-10-31 21:28:12 +0530

    interface: allows to setup shape for objects (only objects)

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,15 @@
-console.log('Here we go')
+// interface : Allows to setup shape for objects (only objects)
+interface Book {
+  readonly isbn: number
+  title: string
+  author: string
+  genre?: string
+}
+const deepWork: Book = {
+  isbn: 123,
+  title: 'deep work',
+  author: 'cal newport',
+  genre: 'self-help'
+}
+
+console.log(deepWork)
