c7cdcde1629596d392909f83f921cfa21685b527
emmeiwhite <emmeiwhite@gmail.com>
2024-10-31 21:30:04 +0530

    Function type  in interface

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -4,12 +4,19 @@ interface Book {
   title: string
   author: string
   genre?: string
+  // method
+  authorDetails(): void
 }
+
 const deepWork: Book = {
   isbn: 123,
   title: 'deep work',
   author: 'cal newport',
-  genre: 'self-help'
+  genre: 'self-help',
+  authorDetails: function () {
+    console.log(this.title)
+    console.log(this.author)
+  }
 }
 
-console.log(deepWork)
+console.log(deepWork.authorDetails())
